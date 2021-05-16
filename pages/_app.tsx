9e31a5250cfb044d76e509f20af1f9b1c51ae50e
Steven Garcia <stevgarcia2015@gmail.com>
2021-05-15 18:32:50 -0700

    Add styes for app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 
 import { ThemeProvider } from "styled-components"
 import { GlobalStyle, theme } from '../shared/theme';
 
+import TopNavbar from "../components/TopNavbar";
 import NavBar from "../components/NavBar";
 import styled from "styled-components";
 
-const Content = styled.div`
+type ContentProps = {
+    isAuthenticated: boolean;
+}
+
+const Content = styled.div<ContentProps>`
     height: 100%;
     overflow-x: hidden;
     overflow-y: scroll;
 
     @media screen and (min-width: 1200px) {
         float: right;
-        width: calc(100% - 350px);
+        width: ${(props) => props.isAuthenticated ? "calc(100% - 350px)" : "100%"};
+        // width: calc(100% - 350px);
     }
 `;
 
 export default function MyApp({ Component, pageProps }) {
+    const [isAuthenticated, setAuthenticated] = useState(false);
+
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle theme={theme} />
@@ -26,8 +34,9 @@ export default function MyApp({ Component, pageProps }) {
                 <title>EasyMealPlanner | Welcome</title>
             </Head>
             <main>
-                <NavBar />
-                <Content>
+                <TopNavbar />
+                {/* <NavBar /> */}
+                <Content isAuthenticated={isAuthenticated}>
                     <Component {...pageProps} />
                 </Content>
             </main>
