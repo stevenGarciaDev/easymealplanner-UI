e3daf9993446a6f2ed5d6e85dea8b0df2fe4a76a
Steven Garcia <stevgarcia2015@gmail.com>
2021-05-15 18:31:28 -0700

    Update login component

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,22 +1,57 @@
-import styled from "styled-components";
-import { AuthForm, FormControl } from "../shared/styles/forms";
+import { useState } from "react";
 import { Center } from "../shared/styles/center";
+import {
+    FormHeadline,
+    FormSubheadline,
+    AuthForm,
+    FormControl,
+    Label,
+    Input
+} from "../shared/styles/forms";
 import { Button } from "../shared/styles/buttons";
 
-const Login = () => (
-    <Center>
-        <h1>Login</h1>
-        <p>Welcome back, let's get you some food.</p>
-        <AuthForm>
-            <FormControl>
-                <input type="email" name="email" />
-            </FormControl>
-            <FormControl>
-                <input type="password" name="password" />
-            </FormControl>
-            <Button>I'm hungry!</Button>
-        </AuthForm>
-    </Center>
-)
+const Login = () => {
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setForm({ ...form, [name]: value });
+    };
+
+    const { email, password } = form;
+    return (
+        <Center>
+            <FormHeadline>Login</FormHeadline>
+            <FormSubheadline>Welcome back, let's get you some food.</FormSubheadline>
+            <AuthForm>
+                <FormControl>
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                        id="email"
+                    />
+                </FormControl>
+                <FormControl>
+                    <Label htmlFor="password">Password</Label>
+                    <Input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                        id="password"
+                    />
+                </FormControl>
+                <Button type="submit">I'm hungry!</Button>
+            </AuthForm>
+        </Center>
+    );
+};
 
 export default Login;
