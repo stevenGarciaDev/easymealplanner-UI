3340f0632e7eb7176adc3595e09a8db049dbdb3b
Steven Garcia <stevgarcia2015@gmail.com>
2021-05-19 10:14:15 -0700

    Update recipe index page

--- pages/recipes-index.tsx
+++ pages/recipes-index.tsx
@@ -1,7 +1,46 @@
+import RecipeSearchBar from "../components/RecipeSearchBar";
+import RecipePreview from "../components/RecipePreview";
+import styled from "styled-components";
+
+const Page = styled.div`
+    position: relative;
+`;
+
+const RecipeContainer = styled.div`
+    display: grid;
+    column-gap: 10px;
+    row-gap: 30px;
+    grid-template-columns: 1fr;
+    justify-items: center;
+    height: 100%;
+    padding: 150px 0px;
+
+    @media screen and (min-width: 700px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media screen and (min-width: 1000px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media screen and (min-width: 1500px) {
+        grid-template-columns: repeat(4, 1fr);
+    }
+`;
+
 const RecipesIndex = () => (
-    <div>
-        RecipesIndex
-    </div>
+    <Page>
+        <RecipeSearchBar />
+        <RecipeContainer>
+            <RecipePreview />
+            <RecipePreview />
+            <RecipePreview />
+            <RecipePreview />
+            <RecipePreview />
+            <RecipePreview />
+            <RecipePreview />
+        </RecipeContainer>
+    </Page>
 );
 
 export default RecipesIndex;
