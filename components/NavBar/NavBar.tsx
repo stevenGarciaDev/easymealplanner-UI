e1e0223a5d7955386c4cd6abf638a16ad84f3522
Steven Garcia <stevgarcia2015@gmail.com>
2021-05-19 10:13:20 -0700

    Update NavBar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,104 +1,21 @@
-import styled from "styled-components";
-import Link from "next/link";
-import { IoFastFood } from "react-icons/io5";
-import { GiCookingPot } from "react-icons/gi";
-import { FaShoppingCart } from "react-icons/fa";
-import { Button } from "../../shared/styles/buttons";
+import SideNavBar from "../SideNavBar";
+import TopNavbar from "../TopNavbar";
+import { connect } from "react-redux";
+import { selectUserToken } from "../../store/user/user.selectors";
 
-const Nav = styled.nav`
-  background-color: #fff;
-  border-right: 2px solid #ccc;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  max-width: 350px;
-  position: fixed;
-  width: 80%;
-  z-index: 2;
-`;
+const NavBar = ({ userToken }) => {
+    console.log("userToken", userToken);
+    return (
+        <>
+            {
+                userToken === '' ? <TopNavbar /> : <SideNavBar />
+            }
+        </>
+    );
+}
 
-const NavTitle = styled.h2`
-  color: #1D8F52;
-  font-size: 3rem;
-  left: 1.5rem;
-  position: relative;
-`;
+const mapStateToProps = (state) => ({
+    userToken: selectUserToken(state)
+});
 
-const NavItemsContainer = styled.div`
-  height: 100%;
-  width: 100%;
-`;
-
-const NavSectionTitle = styled.h2`
-  font-size: 3rem;
-  left: 4rem;
-  margin: 1rem 0;
-  position: relative;
-`;
-
-const SectionLinksContainer = styled.div`
-  color: #656567;
-  font-size: 2.5rem;
-  padding: 5px 50px;
-`;
-
-const NavLink = styled.a`
-  color: #656567;
-  display: flex;
-  margin: 10px 0px;
-
-  &:hover {
-    color: #1D8F52;
-  }
-`;
-
-const NavText = styled.span`
-  padding-left: 10px;
-`;
-
-const NavBar = () => (
-  <Nav>
-      <div>
-        <NavTitle>EasyMealPlanner</NavTitle>
-      </div>
-      <NavItemsContainer>
-        <SectionLinksContainer>
-          <Link href="/create-recipe" passHref>
-            <Button>Add Recipe</Button>
-          </Link>
-        </SectionLinksContainer>
-        <NavSectionTitle>Discover</NavSectionTitle>
-        <SectionLinksContainer>
-          <div>
-            <Link href="/recipes-index" passHref>
-              <NavLink>
-                <IoFastFood />
-                <NavText>Recipes</NavText>
-              </NavLink>
-            </Link>
-          </div>
-        </SectionLinksContainer>
-        <NavSectionTitle>Meal Prep</NavSectionTitle>
-        <SectionLinksContainer>
-          <div>
-            <Link href="/meal-plan" passHref>
-              <NavLink>
-                <GiCookingPot />
-                <NavText>Meal Plan</NavText>
-              </NavLink>
-            </Link>
-          </div>
-          <div>
-            <Link href="/grocery-list" passHref>
-              <NavLink>
-                <FaShoppingCart />
-                <NavText>Shopping List</NavText>
-              </NavLink>
-            </Link>
-          </div>
-        </SectionLinksContainer>
-      </NavItemsContainer>
-  </Nav>
-);
-
-export default NavBar;
+export default connect(mapStateToProps)(NavBar);
