cf5168a6bfeaa945cc40a84319ae72c25a3a6816
Steven Garcia <stevgarcia2015@gmail.com>
2021-05-23 11:14:22 -0700

    Add link for font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Roboto&family=Luckiest+Guy&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Roboto&family=Permanent+Marker&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />
