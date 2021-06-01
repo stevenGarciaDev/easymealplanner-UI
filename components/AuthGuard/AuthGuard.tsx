f8c16f7336be2d26396e80ae00d74e8d37157539
Steven Garcia <stevgarcia2015@gmail.com>
2021-06-01 10:39:13 -0700

    Add higher order component, AuthGuard

---
+++ components/AuthGuard/AuthGuard.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import Router from "next/router";
+import { useSelector } from "react-redux";
+import { selectUserToken } from "../../store/user/user.selectors";
+
+export function AuthGuard({ children }) {
+    const userToken = useSelector(selectUserToken);
+
+    useEffect(() => {
+        if (userToken === '')
+            Router.push('/signup');
+    });
+    
+    return (
+        <>
+            {userToken && <>{children}</>}
+        </>
+    );
+}
---
+++ components/AuthGuard/index.ts
@@ -0,0 +1,1 @@
+export { AuthGuard as default } from "./AuthGuard";
