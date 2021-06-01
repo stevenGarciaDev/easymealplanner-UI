ec66b56d1c70e5c512ffedf175218067bc04b8aa
Steven Garcia <stevgarcia2015@gmail.com>
2021-05-31 18:35:54 -0700

    Update _app to use Redux wrapper and getInitialProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,34 @@
-import React, { useState } from 'react';
-import { Provider } from "react-redux";
-import { store } from "../store/store";
+import React from 'react';
+import { wrapper } from "../store/store";
 import Head from 'next/head';
 import { ThemeProvider } from "styled-components"
 import { GlobalStyle, theme } from '../shared/theme';
 import NavBar from "../components/NavBar";
 import ContentContainer from "../components/ContentContainer";
-import styled from "styled-components";
 
 const MyApp = ({ Component, pageProps }) => {
-    const [isAuthenticated, setAuthenticated] = useState(true);
 
     return (
-        <Provider store={store}>
-            <ThemeProvider theme={theme}>
-                <GlobalStyle theme={theme} />
-                <Head>
-                    <title>EasyMealPlanner | Welcome</title>
-                </Head>
-                <main>
-                        <NavBar />
-                        <ContentContainer>
-                            <Component {...pageProps} />
-                        </ContentContainer>
-                </main>
-            </ThemeProvider>
-        </Provider>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle theme={theme} />
+            <Head>
+                <title>EasyMealPlanner | Welcome</title>
+            </Head>
+            <main>
+                <NavBar />
+                <ContentContainer> 
+                    <Component {...pageProps} />
+                </ContentContainer>
+            </main>
+        </ThemeProvider>
     );
 }
 
-export default MyApp;
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+    const pageProps = Component.getInitialProps
+      ? await Component.getInitialProps(ctx)
+      : {};
+    return { pageProps };
+};
+
+export default wrapper.withRedux(MyApp);
