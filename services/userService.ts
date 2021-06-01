67b34f8336cfe50cf935f38a227a43c5a585e238
Steven Garcia <stevgarcia2015@gmail.com>
2021-05-31 18:37:44 -0700

    Update user service file

---
+++ services/userService.ts
@@ -0,0 +1,25 @@
+import fetch from "node-fetch";
+import { RegisterFormType } from "../shared/types/RegisterFormType";
+import { LoginFormType } from "../shared/types/LoginFormType";
+
+const BASE_URL = process.env.API_URL || "http://localhost:8080/api/v1";
+
+export async function registerUserAsync(registerForm: RegisterFormType) {
+    const response = await fetch(`${BASE_URL}/customers`, {
+        method: 'POST',
+        body: JSON.stringify(registerForm),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    return data;
+}
+
+export async function loginUserAsync(loginForm: LoginFormType) {
+    const response = await fetch(`${BASE_URL}/customers/login`, {
+        method: 'POST',
+        body: JSON.stringify(loginForm),
+        headers: { 'Content-Type': 'application/json' }
+    });
+}
