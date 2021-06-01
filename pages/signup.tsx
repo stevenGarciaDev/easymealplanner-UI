014e823ee6b7b7cb6ebbc78e884aaf252ee9dc1f
Steven Garcia <stevgarcia2015@gmail.com>
2021-06-01 10:41:05 -0700

    Update sign up page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -13,7 +13,10 @@ import {
 import { Button } from "../shared/styles/buttons";
 import { register, setUserErrorMessage} from "../store/user/user.actions";
 import { selectUserToken, selectUserErrorMessage } from "../store/user/user.selectors";
+import { selectLoadingStatus } from "../store/loading/loading.selector";
+import { startLoadingAnimation, stopLoadingAnimation } from "../store/loading/loading.actions";
 import { useDispatch, useSelector } from "react-redux";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const Center = styled.div`
     align-items: center;
@@ -40,6 +43,7 @@ const SignUp = () => {
     const dispatch = useDispatch();
     const token = useSelector(selectUserToken);
     const errorMessage = useSelector(selectUserErrorMessage);
+    const isLoading = useSelector(selectLoadingStatus);
 
     useEffect(() => {
         dispatch(setUserErrorMessage(''));
@@ -52,17 +56,25 @@ const SignUp = () => {
     };
 
     const isValidFormSubmission = () => {
-        return true;
+        for (let key in form) {
+            if (form[key] === '') return false;
+        }
+        return true;   
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
-        dispatch(register(form));
 
-        if (errorMessage === '' && token != '') {
-            Router.push("/recipes-index");
+        if (!isValidFormSubmission()) {
+            dispatch(setUserErrorMessage('Please fill in all form fields.'));
+            return;
         }
+        
+        dispatch(startLoadingAnimation());
+        dispatch(register(form));
+        
+        dispatch(stopLoadingAnimation());
+        Router.push("/recipes-index");
     }
 
     const { username, email, password } = form;
@@ -75,39 +87,43 @@ const SignUp = () => {
                 <FormHeadline>Sign Up</FormHeadline>
                 <FormSubheadline>Find delicious recipes and reach your health goals.</FormSubheadline>
                 {errorMessage && <ErrorText>{errorMessage}</ErrorText>}
-                <AuthForm onSubmit={handleSubmit}>
-                    <FormControl>
-                        <Label htmlFor="username">User name</Label>
-                        <Input
-                            type="text"
-                            name="username"
-                            value={username}
-                            onChange={handleChange}
-                            id="username"
-                        />
-                    </FormControl>
-                    <FormControl>
-                        <Label htmlFor="email">Email</Label>
-                        <Input
-                            type="email"
-                            name="email"
-                            value={email}
-                            onChange={handleChange}
-                            id="email"
-                        />
-                    </FormControl>
-                    <FormControl>
-                        <Label htmlFor="password">Password</Label>
-                        <Input
-                            type="password"
-                            name="password"
-                            value={password}
-                            onChange={handleChange}
-                            id="password"
-                        />
-                    </FormControl>
-                    <Button type="submit">Let's Eat!</Button>
-                </AuthForm>
+                {isLoading ?
+                    <LoadingSpinner />
+                    :
+                    <AuthForm onSubmit={handleSubmit}>
+                        <FormControl>
+                            <Label htmlFor="username">User name</Label>
+                            <Input
+                                type="text"
+                                name="username"
+                                value={username}
+                                onChange={handleChange}
+                                id="username"
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                type="email"
+                                name="email"
+                                value={email}
+                                onChange={handleChange}
+                                id="email"
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <Label htmlFor="password">Password</Label>
+                            <Input
+                                type="password"
+                                name="password"
+                                value={password}
+                                onChange={handleChange}
+                                id="password"
+                            />
+                        </FormControl>
+                        <Button type="submit">Let's Eat!</Button>
+                    </AuthForm>
+                }
             </Center>
         </>
     );
