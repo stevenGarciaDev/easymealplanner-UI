304d81b7942e620a003cbf90b42933bef063cd82
Steven Garcia <stevgarcia2015@gmail.com>
2021-05-31 18:37:23 -0700

    Update NavBar component

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,14 +1,17 @@
+import { useSelector } from "react-redux";
 import SideNavBar from "../SideNavBar";
 import TopNavbar from "../TopNavbar";
-import { connect } from "react-redux";
 import { selectUserToken } from "../../store/user/user.selectors";
 import MobileTopNavbar from "../MobileTopNavbar";
 
-const NavBar = ({ userToken }) => {
+
+const NavBar = () => {
+    const token = useSelector(selectUserToken);
+
     return (
         <>
             {
-                userToken === '' ? 
+                token === '' ? 
                     <TopNavbar /> 
                 : 
                     <>
@@ -20,8 +23,5 @@ const NavBar = ({ userToken }) => {
     );
 }
 
-const mapStateToProps = (state) => ({
-    userToken: selectUserToken(state)
-});
 
-export default connect(mapStateToProps)(NavBar);
+export default NavBar;
