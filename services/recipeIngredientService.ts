9a98f525fb694ff4293379e870fcdc82576c6587
Steven Garcia <stevgarcia2015@gmail.com>
2021-06-02 13:26:56 -0700

    Update api service call for possible ingredient units

--- services/recipeIngredientService.ts
+++ services/recipeIngredientService.ts
@@ -1,10 +1,16 @@
 import fetch from "node-fetch";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = process.env.API_URL || "http://localhost:8080/api/v1";
 
-export async function getPossibleUnitsAsync() {
+export async function getPossibleUnitsAsync(jwt: string) {
     try {
-        const response = await fetch(`${BASE_URL}/api/v1/recipe-ingredients/units`);
+        const response = await fetch(`${BASE_URL}/recipe-ingredients/units`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${jwt}`
+            }
+        });
         return await response.json();
     } catch (error) {
         console.log("error", error);
