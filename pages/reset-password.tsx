2ba6cdde8e4272274301f9bb5db647aa04475378
Steven Garcia <stevgarcia2015@gmail.com>
2021-06-09 10:54:56 -0700

    Add page component for resetting password

---
+++ pages/reset-password.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { Center } from "../shared/styles/center";
+import { 
+    AuthForm,
+    FormSubheadline,
+    FormControl,
+    Label,
+    Input
+ } from "../shared/styles/forms";
+ import { Button } from "../shared/styles/buttons";
+import styled from "styled-components";
+import { FaCheckCircle } from "react-icons/fa";
+
+const PageCenter = styled(Center)`
+    margin-top: 0;
+`;
+
+const SentEmailText = styled(FormSubheadline)`
+    align-items: center;
+    display: flex;
+`;
+
+const CheckIcon = styled(FaCheckCircle)`
+    color: var(--color-primary);
+    font-size: 4rem;
+    margin: 0px 10px;
+    position: relative;
+    top: -5px;
+`;
+
+const ResetPassword = () => {
+    const [email, setEmail] = useState('');
+    const [wasEmailSent, updateEmailStatus] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        updateEmailStatus(true);
+    }
+
+    return (
+        <Center>
+            {wasEmailSent ?
+                <>
+                    <SentEmailText>
+                        <CheckIcon />
+                        <span>An email has been sent for you to reset your password.</span>
+                    </SentEmailText>
+                </>
+                :
+                <>
+                    <FormSubheadline>Recover your user name and reset your password.</FormSubheadline>
+                    <AuthForm onSubmit={handleSubmit}>
+                        <FormControl>
+                            <Label>Email</Label>
+                            <Input
+                                type="email"
+                                value={email}
+                                id={email}
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                        </FormControl>
+                        <Button>Send email</Button>
+                    </AuthForm>
+                </>
+            }
+        </Center>
+    );
+}
+
+export default ResetPassword;
