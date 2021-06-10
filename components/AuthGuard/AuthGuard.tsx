551062b6bff9a9782aad1b9eebe22b97273ed8a6
Steven Garcia <stevgarcia2015@gmail.com>
2021-06-10 10:59:07 -0700

    Get saved recipes in AuthGuard component

--- components/AuthGuard/AuthGuard.tsx
+++ components/AuthGuard/AuthGuard.tsx
@@ -1,15 +1,24 @@
 import { useEffect } from "react";
 import Router from "next/router";
-import { useSelector } from "react-redux";
-import { selectUserToken } from "../../store/user/user.selectors";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUserToken, selectUserId } from "../../store/user/user.selectors";
+import { selectSavedRecipeIds } from "../../store/recipe/recipe.selectors";
+import { getSavedRecipes } from "../../store/recipe/recipe.actions";
 
 export function AuthGuard({ children }) {
     const userToken = useSelector(selectUserToken);
+    const savedRecipes = useSelector(selectSavedRecipeIds);
+    const userId = useSelector(selectUserId);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if (userToken === '')
             Router.push('/login');
-    });
+
+        if (savedRecipes.length === 0) {
+            dispatch(getSavedRecipes(userId, userToken));
+        }
+    }, []);
     
     return (
         <>
