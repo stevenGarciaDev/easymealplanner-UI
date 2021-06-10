71f8e124de8c11c5b9d0181eb60040dc8e23be52
Steven Garcia <stevgarcia2015@gmail.com>
2021-06-10 10:59:31 -0700

    Update recipe index page to save and unsave recipes

--- pages/recipes-index.tsx
+++ pages/recipes-index.tsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { selectUserToken } from "../store/user/user.selectors";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUserToken, selectUserId } from "../store/user/user.selectors";
+import { selectSavedRecipeIds } from "../store/recipe/recipe.selectors";
 import Head from 'next/head';
 import RecipeSearchBar from "../components/RecipeSearchBar";
 import RecipePreview from "../components/RecipePreview";
 import styled from "styled-components";
-import { getPaginatedRecipesAsync } from "../services/recipeService";
+import {
+    getPaginatedRecipesAsync,
+    saveRecipeAsync,
+    unsaveRecipeAsync
+} from "../services/recipeService";
+import { setSavedRecipes } from "../store/recipe/recipe.actions";
 
 const Page = styled.div`
     position: relative;
@@ -35,13 +41,37 @@ const RecipeContainer = styled.div`
 
 const RecipesIndex = () => {
     const userToken = useSelector(selectUserToken);
+    const userId = useSelector(selectUserId);
+    const savedRecipeIds = useSelector(selectSavedRecipeIds);
     const [recipes, setRecipes] = useState([]);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         const response = getPaginatedRecipesAsync(0, 6, userToken)
             .then(data => setRecipes(data));
-            
     }, []);
+
+    const isRecipeSaved = (recipeIndex) => {
+        if (savedRecipeIds === null || savedRecipeIds.length === 0) return false;
+        const foundIndex = savedRecipeIds.indexOf(recipeIndex);
+        console.log("foundIndex", foundIndex);
+
+        return foundIndex !== -1 ? true : false;
+    }
+
+    const saveRecipe = async (recipeId) => {
+        const recipeIdsCopy = [...savedRecipeIds, recipeId];
+        dispatch(setSavedRecipes(recipeIdsCopy));
+
+        await saveRecipeAsync(userId, recipeId, userToken);
+    }
+
+    const unsaveRecipe = async (recipeId) => {
+        const updatedRecipeIds = savedRecipeIds.filter((id) => id !== recipeId);
+        dispatch(setSavedRecipes(updatedRecipeIds));
+
+        await unsaveRecipeAsync(userId, recipeId, userToken);
+    }
 
     return (
         <>
@@ -60,6 +90,9 @@ const RecipesIndex = () => {
                                 id={id}
                                 name={name}
                                 imageLink={mainImageLink}
+                                saved={isRecipeSaved(id)}
+                                saveRecipe={saveRecipe}
+                                unsaveRecipe={unsaveRecipe}
                             />
                         )
                     })}
