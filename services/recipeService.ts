3c4f28d9d2f80ca08ce14eec419eef60591ed762
Steven Garcia <stevgarcia2015@gmail.com>
2021-06-10 10:58:50 -0700

    Update api service calls associated with recipe resource

--- services/recipeService.ts
+++ services/recipeService.ts
@@ -17,6 +17,35 @@ export async function createRecipeAsync(recipeInfo: RecipeType, image: File, tok
     });
 }
 
+export async function saveRecipeAsync(customerId: number, recipeId: number, token: string) {
+    await fetch(`${BASE_URL}/saved-recipes/${recipeId}/${customerId}`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+}
+
+export async function unsaveRecipeAsync(customerId: number, recipeId: number, token: string) {
+    await fetch(`${BASE_URL}/saved-recipes/${recipeId}/${customerId}`, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+}
+
+export async function getSavedRecipesAsync(customerId: number, token: string) {
+    const response = await fetch(`${BASE_URL}/saved-recipes/${customerId}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    const data = await response.json();
+    return data;
+}
+
 export async function getPaginatedRecipesAsync(pageStart: number, pageSize: number, token: string) {
     const response = await fetch(`${BASE_URL}/recipes?page=${pageStart}&size=${pageSize}`, {
         method: 'GET',
