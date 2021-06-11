ed8221cdaf5035c692f1f71907396bf7cc920992
Steven Garcia <stevgarcia2015@gmail.com>
2021-06-10 21:48:05 -0700

    Update AuthGuard to make GET request for recipes amount

--- components/AuthGuard/AuthGuard.tsx
+++ components/AuthGuard/AuthGuard.tsx
@@ -2,13 +2,14 @@ import { useEffect } from "react";
 import Router from "next/router";
 import { useSelector, useDispatch } from "react-redux";
 import { selectUserToken, selectUserId } from "../../store/user/user.selectors";
-import { selectSavedRecipeIds } from "../../store/recipe/recipe.selectors";
-import { getSavedRecipes } from "../../store/recipe/recipe.actions";
+import { selectSavedRecipeIds, selectTotalAmountOfRecipes } from "../../store/recipe/recipe.selectors";
+import { getSavedRecipes, getTotalNumberOfRecipes } from "../../store/recipe/recipe.actions";
 
 export function AuthGuard({ children }) {
     const userToken = useSelector(selectUserToken);
     const savedRecipes = useSelector(selectSavedRecipeIds);
     const userId = useSelector(selectUserId);
+    const totalNumberOfRecipes = useSelector(selectTotalAmountOfRecipes);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -17,6 +18,10 @@ export function AuthGuard({ children }) {
 
         if (savedRecipes.length === 0) {
             dispatch(getSavedRecipes(userId, userToken));
+        }
+
+        if (totalNumberOfRecipes === 0) {
+            dispatch(getTotalNumberOfRecipes(userToken));
         }
     }, []);
     
