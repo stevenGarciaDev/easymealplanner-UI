ed2202ac3f3c2dabb9df036e19772ed2c212e593
Steven Garcia <stevgarcia2015@gmail.com>
2021-06-13 21:54:09 -0700

    Modify RecipeIndex page to use shared styles for recipe grid

--- pages/recipes-index.tsx
+++ pages/recipes-index.tsx
@@ -5,7 +5,6 @@ import { selectSavedRecipeIds, selectTotalAmountOfRecipes } from "../store/recip
 import Head from 'next/head';
 import RecipeSearchBar from "../components/RecipeSearchBar";
 import RecipePreview from "../components/RecipePreview";
-import styled from "styled-components";
 import {
     getPaginatedRecipesAsync,
     getPaginatedRecipesByQueryAsync,
@@ -14,32 +13,7 @@ import {
 } from "../services/recipeService";
 import { setSavedRecipes } from "../store/recipe/recipe.actions";
 import PaginatedPageNumber from "../components/PaginatedPageNumber";
-
-const Page = styled.div`
-    position: relative;
-`;
-
-const RecipeContainer = styled.div`
-    column-gap: 10px;
-    display: grid;
-    grid-template-columns: 1fr;
-    height: 100%;
-    justify-items: center;
-    padding: 150px 10px 50px 10px;
-    grid-gap: 30px;
-
-    @media screen and (min-width: 700px) {
-        grid-template-columns: repeat(2, 1fr);
-    }
-
-    @media screen and (min-width: 1200px) {
-        grid-template-columns: repeat(3, 1fr);
-    }
-
-    @media screen and (min-width: 1600px) {
-        grid-template-columns: repeat(4, 1fr);
-    }
-`;
+import { Page, RecipeContainer } from "../shared/styles/recipeGrid";
 
 const RecipesIndex = () => {
     const userToken = useSelector(selectUserToken);
