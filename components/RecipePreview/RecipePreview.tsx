5d544cbc7db46405e7e7fbaf4ec290acc3227d55
Steven Garcia <stevgarcia2015@gmail.com>
2021-06-13 21:53:19 -0700

    Update RecipePreview component to use shared styles

--- components/RecipePreview/RecipePreview.tsx
+++ components/RecipePreview/RecipePreview.tsx
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Link from "next/link";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
+import {
+  Container,
+  ImageLink,
+  ImageContainer,
+  TextContainer,
+  TextContent,
+  Icon
+} from "../../shared/styles/recipePreview";
 
 const RecipePreview = ({
   id,
@@ -43,52 +51,3 @@ const RecipePreview = ({
 }
 
 export default RecipePreview;
-
-export const Container = styled.div`
-  align-items: center;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  min-height: 300px;
-  min-width: 300px;
-  width: 100%;
-  z-index: 2;
-`;
-
-const ImageLink = styled(Link)`
-  height: 80%;
-`;
-
-const ImageContainer = styled.img`
-  border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
-  cursor: pointer;
-  height: 300px;
-  object-fit: cover;
-  width: 100%;
-`;
-
-const TextContainer = styled.div`
-  border-top: 1px solid #ccc;
-  height: 75px;
-  width: 100%;
-`;
-
-const TextContent = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  font-family: 'Amaranth', sans-serif;
-  font-size: 25px;
-  justify-content: space-between;
-  margin: 0px 10px;
-  position: relative;
-`;
-
-const Icon = styled.div`
-  color: #5f9f3d;
-  cursor: pointer;
-  height: 100%;
-`;
