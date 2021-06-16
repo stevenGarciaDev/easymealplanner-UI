40ca1359ade0282c7ebe568f5e28a67dd26df33b
Steven Garcia <stevgarcia2015@gmail.com>
2021-06-16 15:05:41 -0700

    Update AuthGuard to retrieve meal plan information

--- components/AuthGuard/AuthGuard.tsx
+++ components/AuthGuard/AuthGuard.tsx
@@ -2,24 +2,24 @@ import { useEffect } from "react";
 import Router from "next/router";
 import { useSelector, useDispatch } from "react-redux";
 import { selectUserToken, selectUserId } from "../../store/user/user.selectors";
-import { selectSavedRecipeIds, selectTotalAmountOfRecipes } from "../../store/recipe/recipe.selectors";
+import { selectSavedRecipeIds } from "../../store/recipe/recipe.selectors";
 import { getSavedRecipes, getTotalNumberOfRecipes } from "../../store/recipe/recipe.actions";
+import { getMealPlanIdBasedOnUser, getMealPlanByUserId } from "../../store/meal-plan/meal-plan.actions";
 
 export function AuthGuard({ children }) {
     const userToken = useSelector(selectUserToken);
     const savedRecipes = useSelector(selectSavedRecipeIds);
     const userId = useSelector(selectUserId);
-    const totalNumberOfRecipes = useSelector(selectTotalAmountOfRecipes);
     const dispatch = useDispatch();
 
     useEffect(() => {
         if (userToken === '')
             Router.push('/login');
-
         
         dispatch(getTotalNumberOfRecipes(userToken));
+        dispatch(getMealPlanIdBasedOnUser(userId, userToken));
+        dispatch(getMealPlanByUserId(userId, userToken));
         
-
         if (savedRecipes.length === 0) {
             dispatch(getSavedRecipes(userId, userToken));
         }
